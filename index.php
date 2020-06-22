c737539a34c6ec6d2d5dc672fae3cf2ecc10b107
kiarash zandi <kiarash.gh.zandi@gmail.com>
2020-06-22 13:16:36 +0430

    multiplication table logic

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 echo "multiplication table";
+echo "<table border='1'>";
+for ($i = 1 ; $i <= 10 ; $i++){
+    echo "<tr>";
+    for ($j = 1; $j<= 10; $j++){
+        $result = $i * $j;
+        echo "<td>".$result."</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
